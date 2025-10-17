08d4ffc41d828f23db4220abc3502d254d509b94
Mauricio Mahmud <mauxms1996@gmail.com>
2025-10-17 14:38:40 +0200

    fixed desktop youtube

--- scripts/youtube.js
+++ scripts/youtube.js
@@ -5,9 +5,10 @@
   const utils = await import(chrome.runtime.getURL("scripts/nodesRemover.mjs"));
 
   console.log("Removing shorts from webdesktop youtube.")
-  utils.startRemovingElements(() => [
-    document.getElementById("dismissible"),
-  ], 3000);
+  utils.startRemovingElements(() => {
+    const posibleElements = [...document.getElementsByTagName("ytd-rich-shelf-renderer")];
+    return posibleElements.filter(el => el.hasAttribute("is-shorts"));
+  }, 3000);
 })()
 
 
